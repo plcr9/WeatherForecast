e50c2ed48a336a9e7b34603dba37891bb0b5b940
Paul <118978940+plcr9@users.noreply.github.com>
2023-07-18 18:42:14 +0100

    Update forecast.py

--- forecast.py
+++ forecast.py
@@ -1,1 +1,11 @@
+import requests
 
+def Gen_report(C):
+  url = 'https://wttr.in/{}'.format(C)
+  try:
+    data = requests.get(url)
+    T = data.text
+  except:
+    T = "Error occurred"
+  print(T)
+Gen_report(city_name)
